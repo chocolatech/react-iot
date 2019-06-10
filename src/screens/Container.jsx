dbbd9ede6c7920ff153032a9ca8bfc67c416d8d2
Ewelina Gońda-Warzycha <egonda@sii.pl>
2019-06-10 16:23:38 +0200

    display data

--- src/screens/Container.jsx
+++ src/screens/Container.jsx
@@ -1,41 +1,35 @@
 import React from "react";
-import {Heading} from '../../node_modules/react-bulma-components';
-import {Box} from '../../node_modules/react-bulma-components';
 
-const ScreensContainer = (props) => {
-  return (
-    <div>
-    <Box>
-      <Heading>Title</Heading>
-      <Heading subtitle size={6}>
-        Subtitle
-      </Heading>
-    </Box>
-    <Box>
-      <Heading size={1}>Title</Heading>
-      <Heading subtitle size={3}>
-        Subtitle
-      </Heading>
-    </Box>
-    <Box>
-      <Heading size={2}>Title</Heading>
-      <Heading subtitle size={4} renderAs="h2">
-        Subtitle
-      </Heading>
-    </Box>
-    <Box>
-      <Heading size={3}>Title</Heading>
-      <Heading subtitle size={5} renderAs="h2">
-        Subtitle
-      </Heading>
-    </Box>
-    <Box>
-      <Heading size={4}>Title</Heading>
-      <Heading subtitle size={6} renderAs="h2">
-        Subtitle
-      </Heading>
-    </Box>
-  </div>
-  );
-};
-export default ScreensContainer;
+export default class ScreensContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: [] };
+  }
+
+  getData = () => {
+    const ENDPOINT = `/devices`;
+    fetch(ENDPOINT)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.length);
+        this.setState({ data });
+      });
+  };
+
+  componentDidMount() {
+    this.getData();
+  }
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.data.map(device => (
+          <React.Fragment>
+            <div key={device.id}>{device.name} </div>
+            <p>{device.status}</p>
+            <p>{device.connection}</p>
+          </React.Fragment>
+        ))}
+      </React.Fragment>
+    );
+  }
+}
